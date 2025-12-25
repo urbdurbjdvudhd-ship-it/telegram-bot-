382439bebb88cc1bbd93e31aa6ccd26b5450bde9
urbdurbjdvudhd-ship-it <urbdurbjdvudhd@gmail.com>
2025-12-25 15:53:27 +0100

    Update script to use python3 instead of python

--- start.sh
+++ start.sh
@@ -1,5 +1,1 @@
-#!/data/data/com.termux/files/usr/bin/bash
-cd ~/shopbot_new
-export BOT_TOKEN="8533377036:AAGeIJzwwNh-5Ozc-iiCsC0r6oxTTZPiKMU"
-export ADMIN_ID="7183676619"
-python bot.py
+python3 bot.py
